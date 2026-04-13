35a260e38462fb5022f407020860c5ba116c89c0
LL33ch <s.garazh2002@gmail.com>
2026-04-13 14:06:22 +0300

    fix

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ mkdir -p /etc/dpi-rip
 mkdir -p /usr/lib/lua/luci/controller
 mkdir -p /usr/lib/lua/luci/model/cbi/dpi_rip
 mkdir -p /usr/lib/lua/luci/view/dpi_rip
+mkdir -p /usr/share/rpcd/acl.d
 
 # --- Скачиваем файлы ---
 echo "  -> downloading files"
@@ -26,14 +27,15 @@ dl() {
     wget -q -O "$2" "${RAW}/${PKG}/$1"
 }
 
-dl "files/etc/config/dpi-rip"                /etc/config/dpi-rip
-dl "files/etc/init.d/dpi-rip"               /etc/init.d/dpi-rip
-dl "root/usr/bin/dpi-rip-gen.sh"            /usr/bin/dpi-rip-gen.sh
-dl "luasrc/controller/dpi_rip.lua"           /usr/lib/lua/luci/controller/dpi_rip.lua
-dl "luasrc/model/cbi/dpi_rip/overview.lua"  /usr/lib/lua/luci/model/cbi/dpi_rip/overview.lua
-dl "luasrc/model/cbi/dpi_rip/servers.lua"   /usr/lib/lua/luci/model/cbi/dpi_rip/servers.lua
-dl "luasrc/view/dpi_rip/overview.htm"       /usr/lib/lua/luci/view/dpi_rip/overview.htm
-dl "luasrc/view/dpi_rip/log.htm"            /usr/lib/lua/luci/view/dpi_rip/log.htm
+dl "files/etc/config/dpi-rip"                              /etc/config/dpi-rip
+dl "files/etc/init.d/dpi-rip"                             /etc/init.d/dpi-rip
+dl "root/usr/bin/dpi-rip-gen.sh"                          /usr/bin/dpi-rip-gen.sh
+dl "root/usr/share/rpcd/acl.d/luci-app-dpi-rip.json"     /usr/share/rpcd/acl.d/luci-app-dpi-rip.json
+dl "luasrc/controller/dpi_rip.lua"                         /usr/lib/lua/luci/controller/dpi_rip.lua
+dl "luasrc/model/cbi/dpi_rip/overview.lua"                /usr/lib/lua/luci/model/cbi/dpi_rip/overview.lua
+dl "luasrc/model/cbi/dpi_rip/servers.lua"                 /usr/lib/lua/luci/model/cbi/dpi_rip/servers.lua
+dl "luasrc/view/dpi_rip/overview.htm"                     /usr/lib/lua/luci/view/dpi_rip/overview.htm
+dl "luasrc/view/dpi_rip/log.htm"                          /usr/lib/lua/luci/view/dpi_rip/log.htm
 
 # --- Права ---
 echo "  -> setting permissions"
@@ -43,8 +45,10 @@ chmod +x /usr/bin/dpi-rip-gen.sh
 # --- Включаем сервис ---
 /etc/init.d/dpi-rip enable
 
-# --- Сбрасываем кэш LuCI ---
+# --- Применяем ACL и сбрасываем кэш ---
+/etc/init.d/rpcd restart 2>/dev/null
 rm -rf /tmp/luci-indexcache /tmp/luci-modulecache /tmp/luci-sessions 2>/dev/null
+/etc/init.d/uhttpd restart 2>/dev/null
 
 rm -rf "$TMP"
 
